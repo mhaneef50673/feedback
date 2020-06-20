74503a46ac04402c0f58946e246868fcb0da9f43
Mohamed Haneef Bathuru Jaman <mohamed.jaman@dnata.com>
2020-06-20 20:28:51 +0400

    Add: updated heroku url

--- src/core/api/endpoints.js
+++ src/core/api/endpoints.js
@@ -1,4 +1,4 @@
-export const serviceUrl = 'http://localhost:3001/api/v1/';
+export const serviceUrl = 'https://haneef-feedback-api.herokuapp.com/api/v1/';
 
 export const getFeedbacksUrl = `${serviceUrl}feedbacks`;
 export const postFeedbackUrl = `${serviceUrl}feedback`;
